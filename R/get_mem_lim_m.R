120b37fcb9b51202285168def74950c02ef454b3
Alejandro Ochoa Garcia <alexviiiag@gmail.com>
2019-07-24 14:27:43 -0400

    minor: improved robustness of get_mem_lim_m call in get_A, removed comments, adjusted spacing

--- R/get_mem_lim_m.R
+++ R/get_mem_lim_m.R
@@ -1,5 +1,5 @@
 # an internal constant shared by this function and get_mem_lim
-GB <- 1024*1024*1024
+GB <- 1024 * 1024 * 1024
 
 # mem is in GB
 get_mem_lim_m <- function(n, m = NA, mem = NA, factor = 0.7, verbose = FALSE) {
@@ -19,22 +19,6 @@ get_mem_lim_m <- function(n, m = NA, mem = NA, factor = 0.7, verbose = FALSE) {
         mem <- mem * GB
     }
 
-    # OLD...
-    # estimating total memory usage in bytes, for getMAInt
-    # mem(X) = m*n*4+40 # genotypes are stored as ints!
-    # mem(FHat) = m*8+40 # double vector
-    # fixed cases double X at least temporarily?
-    # X <- X-1 may also double X (but later, not at the same time)
-    # mem(M) = n*n*4+40 # as int (RcppEigen), double otherwise
-    # mem(is.na(X)) same as X
-    # mem(!is.na(X)) same as X
-    # mem(SA) = n*n*4+40 # as int (RcppEigen), double otherwise
-    
-    # mem = # version for int M,SA (RcppEigen), no more FHat
-    # = 2*(m*n*4+40) + 2*(n*n*4+40)
-    # = 2*m*n*4 + 2*n*n*4 + 4*40
-    # = 8*m*n + 8*n*n + 160
-    # = 8*(m*n + n*n + 20)
     # given fixed n, solve for m:
     # get maximum m (number of SNPs) given n and the memory requested
     data <- solve_m_mem_lim(
@@ -44,11 +28,10 @@ get_mem_lim_m <- function(n, m = NA, mem = NA, factor = 0.7, verbose = FALSE) {
         mat_m_n = 1, # X (0.5) + ?
         mat_n_n = 1 # A + M (0.5 + 0.5)
     )
-    #    m_chunk <- (mem/8 - 20 - n*n)/n
 
     if (verbose)
         message('Choice of chunk size should limit mem to about ', round( data$mem_chunk / GB, 2 ), ' GB')
 
-    return(data$m_chunk) # return desired value
+    return( data$m_chunk ) # return desired value
 }
 
